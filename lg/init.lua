a01a6f8c0197345e51ba8866de020fd02303f36b
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2024-11-03 04:00:44 -0300

    Implementation of the update config function

--- lg/init.lua
+++ lg/init.lua
@@ -3,12 +3,10 @@ local debug = require("modules.utils.debug")
 
 -- creates config file
 local config_file = config.generate_config()
+config.update_config(config_file)
 
 local config_values = config.get_config_values(config_file)
-
 debug.show_config_values(config_values)
-
--- config.update_config(config_file)
 
 -- closes config file 
 config.close_config(config_file)

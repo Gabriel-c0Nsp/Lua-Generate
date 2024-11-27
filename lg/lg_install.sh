ae14847e602240b81656b21465d5686dd756a233
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2024-11-27 10:31:48 -0300

    some changes on the installation script

--- lg/lg_install.sh
+++ lg/lg_install.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Colors of the messages
+red='\e[0;31m'
+green='\e[0;32m'
+yellow='\e[0;33m'
+reset='\e[0m'
+
 LG_PATH=$(pwd)
 LG_MODULES=$LG_PATH/modules
 
@@ -13,3 +19,5 @@ chmod +x ./lg
 
 echo "#!/bin/sh" >> lg
 echo "lua5.3 $LG_PATH/init.lua \"\$@\"" >> lg
+
+echo -e "${green}Lua Generate has been installed successfully!${reset}"

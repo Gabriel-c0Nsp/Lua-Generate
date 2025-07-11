b22138e8b1cb97aa2d94ceba4a3379c3ec00b036
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2025-07-11 18:00:36 -0300

    now the installation script determines if the user has a .zshrc file or a .bashrc file

--- lg/lg_install.sh
+++ lg/lg_install.sh
@@ -22,7 +22,19 @@ reset='\e[0m'
 
 LG_PATH=$(pwd)
 LG_MODULES=$LG_PATH/modules
-CHOICE="" # stores user's choice
+CHOICE="" # to store user's choice
+SHELL_FILE=""
+
+if test -f "$HOME"/.zshrc; then
+  SHELL_FILE=".zshrc"
+elif test -f "$HOME"/.bashrc; then
+  SHELL_FILE=".bashrc"
+else
+  echo -e "${red}Looks like you don't have a Shell Configuration File like: .bashrc or .zshrc${reset}"
+  echo "You could proced with the manual installation."
+  echo -e "See ${blue}https://github.com/Gabriel-c0Nsp/Lua-Generate${reset} for more information."
+  return 1
+fi
 
 # check if the user has the right version of lua
 if ! lua5.3 -v >/dev/null 2>&1 ; then
@@ -42,7 +54,7 @@ if test -f lg; then
     return 0
   fi
 else 
-  if grep "export PATH=\$PATH:.*/lg" "$HOME"/.bashrc >/dev/null 2>&1 ; then
+  if grep "export PATH=\$PATH:.*/lg" "$HOME/$SHELL_FILE" >/dev/null 2>&1 ; then
     # The user probably has a conflicting program inside their system
     echo -e "${red}You already have a lg named script defined on your system!${reset}"
     echo -e "If it's not the Lua Generate program, consider renaming or removing it.\n"
@@ -64,11 +76,11 @@ echo "#!/bin/sh" > lg
 echo "lua5.3 $LG_PATH/init.lua \"\$@\"" >> lg
 
 # make lg command executable everywhere inside the user's terminal
-echo -e "\n# Inserted by Lua Generate program:" >> ${HOME}/.bashrc
-echo -e "export PATH=\$PATH:${LG_PATH}" >> ${HOME}/.bashrc
+echo -e "\n# Inserted by Lua Generate program:" >> "$HOME/$SHELL_FILE"
+echo -e "export PATH=\$PATH:${LG_PATH}" >> "$HOME/$SHELL_FILE"
 
 echo -e "${green}Lua Generate has been installed successfully!${reset}"
 echo "You can try: lg --help for more information"
 
-# sourcing the .bashrc user's file so it can be executable right away
-source ${HOME}/.bashrc
+# sourcing the user's shell configuration file so it can be executable right away
+source "$HOME/$SHELL_FILE"
